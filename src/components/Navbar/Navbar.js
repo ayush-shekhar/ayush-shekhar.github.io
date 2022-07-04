c55febe224d29668aef7b15146e1b3086275fde8
raj-sellgo26 <raj@sellgo.com>
2022-07-04 17:02:37 +0530

    Update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded'
 import MenuIcon from '@material-ui/icons/Menu'
 import CloseIcon from '@material-ui/icons/Close'
 import { ThemeContext } from '../../contexts/theme'
-import { projects, skills, contact } from '../../portfolio'
+import { skills, contact } from '../../portfolio'
 import './Navbar.css'
 
 const Navbar = () => {
@@ -19,18 +19,6 @@ const Navbar = () => {
         style={{ display: showNavList ? 'flex' : null }}
         className='nav__list'
       >
-        {projects.length ? (
-          <li className='nav__list-item'>
-            <a
-              href='#projects'
-              onClick={toggleNavList}
-              className='link link--nav'
-            >
-              Projects
-            </a>
-          </li>
-        ) : null}
-
         {skills.length ? (
           <li className='nav__list-item'>
             <a
